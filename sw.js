ec6a72a706a54f070eb03f235a032523c51f7fbc
haroldco45 <68300211+haroldco45@users.noreply.github.com>
2026-04-19 04:09:03 -0500

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'tiendaerp-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Sans:wght@300;400;500&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
